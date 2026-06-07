83be289c21c51fa0f2a59dc5fbe5ed99b3698b84
Antony Alvin Johnson <123061519+antony561@users.noreply.github.com>
2026-06-07 13:11:24 +1000

    Update ddl_gold.sql

--- Scripts/gold/ddl_gold.sql
+++ Scripts/gold/ddl_gold.sql
@@ -68,15 +68,15 @@ WHERE prd_end_dt IS NULL --- Filtering out all Historial data
 GO
 CREATE VIEW gold.fact_sales AS
 SELECT 
-s.sls_ord_num AS order_number,
-p.product_key,
-c.customer_key,
-s.sls_order_dt AS order_date,
-s.sls_ship_dt AS ship_date,
-s.sls_due_dt AS due_date,
-s.sls_sales AS sales_amount,
-s.sls_quantity AS quantity,
-s.sls_price AS price
+	s.sls_ord_num AS order_number,
+	p.product_key,
+	c.customer_key,
+	s.sls_order_dt AS order_date,
+	s.sls_ship_dt AS ship_date,
+	s.sls_due_dt AS due_date,
+	s.sls_sales AS sales_amount,
+	s.sls_quantity AS quantity,
+	s.sls_price AS price
 FROM silver.crm_sales_details s
 LEFT JOIN gold.dim_products p
 ON s.sls_prd_key = p.product_number
